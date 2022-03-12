88bada468ef025771dfc65f609db4b2e938ee118
Raymar <raymarmontecs@gmail.com>
2022-03-13 01:00:04 +0800

    Changes to TableRobot: - Renamed to ToyRobot - Changed constructor to accept Vector object directly

---
+++ app/src/main/java/toyrobot/robot/ToyRobot.java
@@ -0,0 +1,68 @@
+package toyrobot.robot;
+
+import java.util.EnumMap;
+
+import toyrobot.util.Coordinates;
+import toyrobot.util.Direction;
+import toyrobot.util.Vector;
+
+/**
+ * Implementation of a robot that gets placed on a table, can move one step
+ * on the direction it's facing, can face left, and can face right.
+ */
+public class ToyRobot implements IRobot {
+
+    private Vector details;
+
+    /** 
+     * Map of the forward displacement that will be done depending on the
+     * direction that is being faced.
+     */
+    private static final EnumMap<Direction, Coordinates> movementMap =
+        createMovementMap();
+
+    public ToyRobot(Vector details) {
+        this.details = details;
+    }
+
+    @Override
+    public void move() {
+        details.setCoordinates(nextMovePosition());
+    }
+
+    @Override
+    public void rotateLeft() {
+        details.setOrientation(Direction.leftOf(details.getOrientation()));
+    }
+
+    @Override
+    public void rotateRight() {
+        details.setOrientation(Direction.rightOf(details.getOrientation()));
+        
+    }
+
+    @Override
+    public Coordinates nextMovePosition() {
+        Coordinates movement = movementMap.get(details.getOrientation());
+        return Coordinates.addCoordinates(
+            details.getCoordinates(), movement);
+    }
+
+    /**
+     * Returns a copy of the the current details of the robot.
+     */
+    @Override
+    public Vector getDetails() {
+        return new Vector(details);
+    }
+    
+    private static EnumMap<Direction, Coordinates> createMovementMap() {
+        EnumMap<Direction, Coordinates> movementMap = new EnumMap<>(Direction.class);
+        movementMap.put(Direction.NORTH, new Coordinates(0, 1));
+        movementMap.put(Direction.EAST, new Coordinates(1, 0));
+        movementMap.put(Direction.SOUTH, new Coordinates(0, -1));
+        movementMap.put(Direction.WEST, new Coordinates(-1, 0));
+        return movementMap;
+    }
+
+}
