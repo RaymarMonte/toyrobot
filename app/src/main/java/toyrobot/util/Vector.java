eb675861efad865b74ac1d6d77db3cf464e6fdc1
Raymar <raymarmontecs@gmail.com>
2022-03-13 00:51:05 +0800

    Changes to RobotData: - Renamed to Vector - Added copy constructor - Transferred to util folder

---
+++ app/src/main/java/toyrobot/util/Vector.java
@@ -0,0 +1,66 @@
+package toyrobot.util;
+
+/**
+ * An entity that has coordinates and a direction it's facing.
+ */
+public class Vector {
+    
+    private Coordinates coordinates;
+    private Direction orientation;
+    
+    public Vector(Coordinates position, Direction orientation) {
+        this.coordinates = position;
+        this.orientation = orientation;
+    }
+
+    /** Copy constructor */
+    public Vector(Vector toCopy) {
+        this.coordinates = new Coordinates(toCopy.coordinates);
+        this.orientation = toCopy.orientation;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public Direction getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(Direction orientation) {
+        this.orientation = orientation;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((orientation == null) ? 0 : orientation.hashCode());
+        result = prime * result + ((coordinates == null) ? 0 : coordinates.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vector other = (Vector) obj;
+        if (orientation != other.orientation)
+            return false;
+        if (coordinates == null) {
+            if (other.coordinates != null)
+                return false;
+        } else if (!coordinates.equals(other.coordinates))
+            return false;
+        return true;
+    }
+    
+}
