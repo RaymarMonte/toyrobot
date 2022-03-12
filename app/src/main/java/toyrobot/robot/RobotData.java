ca7dfd4f8129f6c5a6cebf425b0f464a3d4d1449
Raymar <raymarmontecs@gmail.com>
2022-03-12 16:01:54 +0800

    Added hashCode() and equals() overrides on RobotData and RobotPosition

--- app/src/main/java/toyrobot/robot/RobotData.java
+++ app/src/main/java/toyrobot/robot/RobotData.java
@@ -29,4 +29,32 @@ public class RobotData {
         this.facing = facing;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((facing == null) ? 0 : facing.hashCode());
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RobotData other = (RobotData) obj;
+        if (facing != other.facing)
+            return false;
+        if (position == null) {
+            if (other.position != null)
+                return false;
+        } else if (!position.equals(other.position))
+            return false;
+        return true;
+    }
+    
 }
--- app/src/main/java/toyrobot/robot/RobotPosition.java
+++ app/src/main/java/toyrobot/robot/RobotPosition.java
@@ -25,5 +25,30 @@ public class RobotPosition {
     public void setPositionY(int positionY) {
         this.positionY = positionY;
     }
-    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + positionX;
+        result = prime * result + positionY;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RobotPosition other = (RobotPosition) obj;
+        if (positionX != other.positionX)
+            return false;
+        if (positionY != other.positionY)
+            return false;
+        return true;
+    }
+
 }
