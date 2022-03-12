ca53f3581eeac112c3ee86656d1b3c08fc0d2185
Raymar <raymarmontecs@gmail.com>
2022-03-12 11:06:02 +0800

    Converted Robot.java into an interface

--- app/src/main/java/toyrobot/Robot.java
+++ app/src/main/java/toyrobot/Robot.java
@@ -1,44 +1,11 @@
 package toyrobot;
 
-/**
- * Represents a robot that has an x and y position and faces a particular
- * direction.
- */
-public class Robot {
+public interface Robot {
     
-    private int positionX;
-    private int positionY;
-    /** The direction that the robot is facing. */
-    private Direction facing;
-    
-    public Robot(int positionX, int positionY, Direction facing) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.facing = facing;
-    }
-
-    public int getPositionX() {
-        return positionX;
-    }
-
-    public void setPositionX(int positionX) {
-        this.positionX = positionX;
-    }
-
-    public int getPositionY() {
-        return positionY;
-    }
-
-    public void setPositionY(int positionY) {
-        this.positionY = positionY;
-    }
-
-    public Direction getFacing() {
-        return facing;
-    }
-
-    public void setFacing(Direction facing) {
-        this.facing = facing;
-    }
+    public void move();
+    public void rotateLeft();
+    public void rotateRight();
+    public RobotPosition nextMovePosition();
+    public RobotData getCurrentData();
 
 }
