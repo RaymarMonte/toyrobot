0973a95bbd84466fa34ae5d43a5da0749952107c
Raymar <raymarmontecs@gmail.com>
2022-03-12 15:38:49 +0800

    Implemented Direction and DirectionTest

--- app/src/main/java/toyrobot/Direction.java
+++ app/src/main/java/toyrobot/Direction.java
@@ -1,5 +1,36 @@
 package toyrobot;
 
+/**
+ * Represents directions commonly found in compasses. The order in which
+ * these directions are laid out follows their equivalent value in degrees,
+ * starting with North which has the value of 0 degrees.
+ */
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
+
+    public static Direction leftOf(Direction direction) {
+        int index = direction.ordinal();
+        int leftIndex = index - 1;
+        return Direction.values()[normalizeIndex(leftIndex)];
+    }
+
+    public static Direction rightOf(Direction direction) {
+        int index = direction.ordinal();
+        int leftIndex = index + 1;
+        return Direction.values()[normalizeIndex(leftIndex)];
+    }
+
+    /** 
+     * Normalizes a computed index in a circular manner.
+     * Values larger than max length loops back to near 0.
+     * Negative values loops back to near max length.
+     */
+    private static int normalizeIndex(int index) {
+        int directionCount = Direction.values().length;
+        return (index + directionCount) % directionCount;
+    }
+    
 }
---
+++ app/src/test/java/toyrobot/DirectionTest.java
@@ -0,0 +1,17 @@
+package toyrobot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class DirectionTest {
+    
+    @Test void leftOfNorthShouldBeWest() {
+        assertEquals(Direction.WEST, Direction.leftOf(Direction.NORTH));
+    }
+    
+    @Test void rightOfWestShouldBeNorth() {
+        assertEquals(Direction.NORTH, Direction.rightOf(Direction.WEST));
+    }
+
+}
