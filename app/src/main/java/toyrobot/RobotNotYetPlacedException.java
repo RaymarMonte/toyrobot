290261ed4de8665b3de2a268eee55a29a6ddfc25
Raymar <raymarmontecs@gmail.com>
2022-03-12 13:13:19 +0800

    Moved exceptions to exceptions folder

--- app/src/main/java/toyrobot/RobotNotYetPlacedException.java
+++
@@ -1,9 +0,0 @@
-package toyrobot;
-
-public class RobotNotYetPlacedException extends IllegalStateException {
-    
-    public RobotNotYetPlacedException() {
-        super("Unable to do action while Robot is not yet placed.");
-    }
-
-}
--- app/src/main/java/toyrobot/RobotOutOfBoundsException.java
+++
@@ -1,9 +0,0 @@
-package toyrobot;
-
-public class RobotOutOfBoundsException extends IllegalArgumentException {
-    
-    public RobotOutOfBoundsException() {
-        super("Unable to do action that will put Robot out of bounds.");
-    }
-
-}
