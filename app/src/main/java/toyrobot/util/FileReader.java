f549852762fac71efd008ac232264c04fca6d5b5
Raymar <raymarmontecs@gmail.com>
2022-03-13 16:55:17 +0800

    Added FileReader Utility class

---
+++ app/src/main/java/toyrobot/util/FileReader.java
@@ -0,0 +1,27 @@
+package toyrobot.util;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * Utility class for reading a file.
+ */
+public class FileReader {
+    
+    public static String[] readFileToStringArray(String filePath) {
+        String[] stringArray = new String[0];
+        Path path = FileSystems.getDefault().getPath(filePath);
+        try {
+            Stream<String> lines = Files.lines(path);
+            stringArray = (String[]) lines.toArray();
+            lines.close();
+        } catch (IOException e) {
+            // Ignore
+        }
+        return stringArray;
+    }
+
+}
