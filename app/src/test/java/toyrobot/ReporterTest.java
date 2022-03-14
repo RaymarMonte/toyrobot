02b18dd4505562feef12ebf62ae9298174ffb33e
Raymar <raymarmontecs@gmail.com>
2022-03-15 07:52:57 +0800

    Completed ReporterTest test class

---
+++ app/src/test/java/toyrobot/ReporterTest.java
@@ -0,0 +1,19 @@
+package toyrobot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import toyrobot.entities.CoordinatesWithDirection;
+import toyrobot.entities.Direction;
+import toyrobot.services.reporter.Reporter;
+
+public class ReporterTest extends SysOutTest {
+    
+    @Test void reportingPrintsToSystemOut() {
+        Reporter reporter = new Reporter();
+        reporter.report(new CoordinatesWithDirection(3, 3, Direction.WEST));
+        assertEquals("3,3,WEST".trim(), outContent.toString().trim());
+    }
+
+}
