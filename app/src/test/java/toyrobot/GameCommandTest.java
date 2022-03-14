dca5358eb77bd586c5d2c4438767b8d9ec48c0c6
Raymar <raymarmontecs@gmail.com>
2022-03-14 22:44:40 +0800

    Replace TableCommandTest with GameCommandTest

---
+++ app/src/test/java/toyrobot/GameCommandTest.java
@@ -0,0 +1,19 @@
+package toyrobot;
+
+import org.junit.jupiter.api.Test;
+
+public class GameCommandTest {
+    
+    @Test void executeValidCommandWithCorrectParams() {
+
+    }
+
+    @Test void executeInvalidCommand() {
+
+    }
+
+    @Test void executeValidCommandWithInvalidParams() {
+        
+    }
+
+}
