1786798606bd2d3d2211ee359bf9ae06b6693340
Raymar <raymarmontecs@gmail.com>
2022-03-14 20:03:21 +0800

    Deleted TableTest since it has been replaced by GameTest

--- app/src/test/java/toyrobot/GameTest.java
+++ app/src/test/java/toyrobot/GameTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import toyrobot.entities.CoordinatesFacingADirection;
+import toyrobot.entities.CoordinatesWithDirection;
 import toyrobot.entities.Direction;
 import toyrobot.game.Game;
 import toyrobot.reporter.IReporter;
@@ -48,14 +48,14 @@ public class GameTest extends SysOutTest {
 
     @Test void gameCanPlaceInsideBoard() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(0, 0, Direction.NORTH));
+        game.place(new CoordinatesWithDirection(0, 0, Direction.NORTH));
         game.report();
         assertEquals("0,0,NORTH".trim(), outContent.toString().trim());
     }
 
     @Test void gameCanMoveAfterPlace() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(1, 1, Direction.EAST));
+        game.place(new CoordinatesWithDirection(1, 1, Direction.EAST));
         game.move();
         game.report();
         assertEquals("2,1,EAST".trim(), outContent.toString().trim());
@@ -63,7 +63,7 @@ public class GameTest extends SysOutTest {
 
     @Test void gameCanLeftAfterPlace() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(2, 2, Direction.SOUTH));
+        game.place(new CoordinatesWithDirection(2, 2, Direction.SOUTH));
         game.left();
         game.report();
         assertEquals("2,2,EAST".trim(), outContent.toString().trim());
@@ -71,7 +71,7 @@ public class GameTest extends SysOutTest {
 
     @Test void gameCanRightAfterPlace() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(3, 3, Direction.WEST));
+        game.place(new CoordinatesWithDirection(3, 3, Direction.WEST));
         game.right();
         game.report();
         assertEquals("3,3,NORTH".trim(), outContent.toString().trim());
@@ -79,21 +79,21 @@ public class GameTest extends SysOutTest {
 
     @Test void gameCanReportAfterPlace() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(4, 4, Direction.NORTH));
+        game.place(new CoordinatesWithDirection(4, 4, Direction.NORTH));
         game.report();
         assertEquals("4,4,NORTH".trim(), outContent.toString().trim());
     }
 
     @Test void gameCantPlaceOutsideBoardDimensions() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(5, 5, Direction.EAST));
+        game.place(new CoordinatesWithDirection(5, 5, Direction.EAST));
         game.report();
         assertEquals("".trim(), outContent.toString().trim());
     }
 
     @Test void gameCantMoveOutsideBoardDimensions() {
         Game game = new Game(reporter);
-        game.place(new CoordinatesFacingADirection(0, 0, Direction.SOUTH));
+        game.place(new CoordinatesWithDirection(0, 0, Direction.SOUTH));
         game.move();
         game.report();
         assertEquals("0,0,SOUTH".trim(), outContent.toString().trim());
