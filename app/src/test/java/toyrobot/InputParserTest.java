726c486e4e54efa8fbc81fc58bf0cb58d510a0bc
Raymar <raymarmontecs@gmail.com>
2022-03-15 07:46:39 +0800

    Completed InputParserTest test class

--- app/src/test/java/toyrobot/InputParserTest.java
+++ app/src/test/java/toyrobot/InputParserTest.java
@@ -1,15 +1,26 @@
 package toyrobot;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import toyrobot.domains.game.IGame;
+import toyrobot.services.inputparser.InputParser;
 
 public class InputParserTest {
     
-    @Test void parsingValidInputProducesGameCommand() {
+    private IGame game = Mockito.mock(IGame.class);
 
+    @Test void parsingValidInputProducesGameCommand() {
+        InputParser inputParser = new InputParser();
+        assertNotNull(inputParser.parseCommand("PLACE 1,2,NORTH", game));
     }
 
     @Test void parsingInvalidInputDoesNotProduceGameCommand() {
-        
+        InputParser inputParser = new InputParser();
+        assertNull(inputParser.parseCommand("PLACE 1", game));
     }
 
 }
