2c8f7c37729f6bf18e92a7e9d2d1d56e774b7a4f
Raymar <raymarmontecs@gmail.com>
2022-03-14 23:15:10 +0800

    Added InputParserTest test class

---
+++ app/src/test/java/toyrobot/InputParserTest.java
@@ -0,0 +1,15 @@
+package toyrobot;
+
+import org.junit.jupiter.api.Test;
+
+public class InputParserTest {
+    
+    @Test void parsingValidInputProducesGameCommand() {
+
+    }
+
+    @Test void parsingInvalidInputDoesNotProduceGameCommand() {
+        
+    }
+
+}
