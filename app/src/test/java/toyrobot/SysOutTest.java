e827d2b3dcaae2beaa33826251c4fc4d11927b8c
Raymar <raymarmontecs@gmail.com>
2022-03-14 19:31:58 +0800

    Implemented SysOutTest.java

---
+++ app/src/test/java/toyrobot/SysOutTest.java
@@ -0,0 +1,29 @@
+package toyrobot;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Abstract class that takes care of setting up a test class for testing
+ * behaviors that involve printing out to System.out.
+ */
+public abstract class SysOutTest {
+    
+    protected final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    private void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    private void restoreStreams() {
+        System.setOut(originalOut);
+    }
+    
+
+}
