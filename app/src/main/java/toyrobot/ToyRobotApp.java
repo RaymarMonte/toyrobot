e494ebe19ab658b6b386ab40aa4cf5c18e128f90
Raymar <raymarmontecs@gmail.com>
2022-03-15 21:32:21 +0800

    Completed ToyRobotApp class

--- app/src/main/java/toyrobot/ToyRobotApp.java
+++ app/src/main/java/toyrobot/ToyRobotApp.java
@@ -1,12 +1,34 @@
 package toyrobot;
 
+import toyrobot.client.GameClient;
+import toyrobot.domains.game.Game;
+import toyrobot.domains.game.IGame;
+import toyrobot.services.inputparser.IInputParser;
+import toyrobot.services.inputparser.InputParser;
+import toyrobot.services.inputsource.IInputSource;
+import toyrobot.services.inputsource.InputSource;
+import toyrobot.services.reporter.IReporter;
+import toyrobot.services.reporter.Reporter;
+
 /**
- * TODO: Recheck name again
+ * Initializes the Toy Robot Application.
  */
 public class ToyRobotApp {
     
-    public void start() {
+    private IInputSource inputSource;
 
+    public ToyRobotApp(String[] args) {
+        IReporter reporter = new Reporter();
+        IGame game = new Game(reporter);
+        IInputParser inputParser = new InputParser();
+        GameClient gameClient = new GameClient(game, inputParser);
+        this.inputSource = new InputSource(gameClient, args);
     }
+
+    public void run() {
+        inputSource.getInputs();
+    }
+
+
 
 }
