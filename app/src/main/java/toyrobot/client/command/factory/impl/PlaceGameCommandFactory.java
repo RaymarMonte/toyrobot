d35ac788f467ffc9d707c98a10b10c9fcd58f7c0
Raymar <raymarmontecs@gmail.com>
2022-03-16 13:44:32 +0800

    Implemented PlaceGameCommandFactory and SimpleGameCommandFactory

---
+++ app/src/main/java/toyrobot/client/command/factory/impl/PlaceGameCommandFactory.java
@@ -0,0 +1,13 @@
+package toyrobot.client.command.factory.impl;
+
+import toyrobot.client.command.command.IGameCommand;
+import toyrobot.client.command.command.impl.PlaceGameCommand;
+import toyrobot.domains.game.IGame;
+
+public class PlaceGameCommandFactory extends GameCommandFactory {
+
+    public IGameCommand createGameCommand(IGame game, String[] params) {
+        return new PlaceGameCommand(game, params);
+    }
+
+}
